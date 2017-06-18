487a1e4054e551e327f78e18ec8a7415a0676a99
Sorin Guga <sorin.guga@e-spres-oh.com>
2017-06-18 19:17:53 +0300

    Moved functions around to have a more sensical top-down hierarchy.

--- PREi.cpp
+++ PREi.cpp
@@ -23,9 +23,111 @@ void PREi::run() {
   yield();
 }
 
+void PREi::init() {
+  Serial.begin(115200);
+  WiFi.setAutoConnect(true);
+  ESPhttpUpdate.rebootOnUpdate(false);
+
+  WiFi.mode(WIFI_AP_STA);
+  if(_ap_password.length() > 7 && _ap_password.length() < 64) {
+    WiFi.softAP(_esp_hostname.c_str(), _ap_password.c_str());
+  } else {
+    WiFi.softAP(_esp_hostname.c_str());
+  }
+
+  _dns_server.setErrorReplyCode(DNSReplyCode::NoError);
+  _dns_server.start(53, "*", WiFi.softAPIP());
+
+  MDNS.begin(_esp_hostname.c_str());
+
+  _web_server.on("/esp", HTTP_GET, std::bind(&PREi::handleInfo, this));
+  _web_server.on("/esp", HTTP_POST, std::bind(&PREi::handleUpdate, this));
+  _web_server.on("/esp", HTTP_DELETE, std::bind(&PREi::handleRestart, this));
+
+  _web_server.on("/wifi", HTTP_GET, std::bind(&PREi::handleScan, this));
+  _web_server.on("/wifi", HTTP_POST, std::bind(&PREi::handleConnect, this));
+  _web_server.on("/wifi", HTTP_DELETE, std::bind(&PREi::handleDisconnect, this));
+
+  _web_server.begin();
+}
+
 void PREi::sendJSON(int code, String message, bool raw=false) {
   String success = (code/100) == 2 ? "true" : "false";
   _web_server.send(code, "application/json", raw ? message : "{\"success\":" + success+ ",\"message\":\"" + message+ "\"}");
+}
+
+void PREi::handleInfo() {
+  PREi::sendJSON(200, generateInfoJSON(), true);
+}
+
+void PREi::handleUpdate() {
+  HTTPUpdateResult ret = ESPhttpUpdate.update(_web_server.arg("firmware"), VERSION);
+  switch(ret) {
+      case HTTP_UPDATE_FAILED:
+          PREi::sendJSON(500, "Update failed.");
+          break;
+      case HTTP_UPDATE_NO_UPDATES:
+          PREi::sendJSON(304, "Update not necessary.");
+          break;
+      case HTTP_UPDATE_OK:
+          PREi::sendJSON(200, "Update started.");
+          ESP.restart();
+          break;
+  }
+}
+
+void PREi::handleRestart() {
+  PREi::sendJSON(200, "Restart initiated succesfully!");
+  ESP.restart();
+}
+
+void PREi::handleScan() {
+  _web_server.send(200, "application/json", generateScanJSON());
+}
+
+void PREi::handleConnect() {
+  const int MAX_TRIES = 30;
+  int status;
+  String ssid = _web_server.arg("ssid");
+  String pass = _web_server.arg("pass");
+
+  int respCode = 200;
+  String respMessage;
+
+  if(ssid.length() > 31 || ssid.length() == 0) {
+    respCode = 400;
+    respMessage = "Invalid SSID provided.";
+  } else if((pass.length() > 0 && pass.length() < 8) || pass.length() > 63) {
+    respCode = 400;
+    respMessage = "Invalid password provided.";
+  }
+
+  if(respCode == 200) {
+    WiFi.begin(ssid.c_str(), pass.c_str());
+
+    for(int t=0; t<MAX_TRIES && (status = WiFi.status()) != WL_CONNECTED; ++t) {
+      delay(200);
+    }
+
+    if(status != WL_CONNECTED) {
+      respCode = 403;
+      respMessage = "Invalid credentials provided.";
+    } else {
+      respCode = 200;
+      respMessage = "Connected succesfully to provided WiFi.";
+    }
+  }
+
+  PREi::sendJSON(respCode, respMessage);
+
+  if(respCode/100 != 2) {
+    WiFi.disconnect();
+  }
+}
+
+void PREi::handleDisconnect() {
+  PREi::sendJSON(200, "Disconnected succesfully!");
+  WiFi.disconnect();
 }
 
 String PREi::generateInfoJSON() {
@@ -126,106 +228,4 @@ String PREi::generateScanJSON() {
   }
 
   return JSON + "]}";
-}
-
-void PREi::init() {
-  Serial.begin(115200);
-  WiFi.setAutoConnect(true);
-  ESPhttpUpdate.rebootOnUpdate(false);
-
-  WiFi.mode(WIFI_AP_STA);
-  if(_ap_password.length() > 7 && _ap_password.length() < 64) {
-    WiFi.softAP(_esp_hostname.c_str(), _ap_password.c_str());
-  } else {
-    WiFi.softAP(_esp_hostname.c_str());
-  }
-
-  _dns_server.setErrorReplyCode(DNSReplyCode::NoError);
-  _dns_server.start(53, "*", WiFi.softAPIP());
-
-  MDNS.begin(_esp_hostname.c_str());
-
-  _web_server.on("/esp", HTTP_GET, std::bind(&PREi::handleInfo, this));
-  _web_server.on("/esp", HTTP_POST, std::bind(&PREi::handleUpdate, this));
-  _web_server.on("/esp", HTTP_DELETE, std::bind(&PREi::handleRestart, this));
-
-  _web_server.on("/wifi", HTTP_GET, std::bind(&PREi::handleScan, this));
-  _web_server.on("/wifi", HTTP_POST, std::bind(&PREi::handleConnect, this));
-  _web_server.on("/wifi", HTTP_DELETE, std::bind(&PREi::handleDisconnect, this));
-
-  _web_server.begin();
-}
-
-void PREi::handleInfo() {
-  PREi::sendJSON(200, generateInfoJSON(), true);
-}
-
-void PREi::handleUpdate() {
-  HTTPUpdateResult ret = ESPhttpUpdate.update(_web_server.arg("firmware"), VERSION);
-  switch(ret) {
-      case HTTP_UPDATE_FAILED:
-          PREi::sendJSON(500, "Update failed.");
-          break;
-      case HTTP_UPDATE_NO_UPDATES:
-          PREi::sendJSON(304, "Update not necessary.");
-          break;
-      case HTTP_UPDATE_OK:
-          PREi::sendJSON(200, "Update started.");
-          ESP.restart();
-          break;
-  }
-}
-
-void PREi::handleRestart() {
-  PREi::sendJSON(200, "Restart initiated succesfully!");
-  ESP.restart();
-}
-
-void PREi::handleScan() {
-  _web_server.send(200, "application/json", generateScanJSON());
-}
-
-void PREi::handleConnect() {
-  const int MAX_TRIES = 30;
-  int status;
-  String ssid = _web_server.arg("ssid");
-  String pass = _web_server.arg("pass");
-
-  int respCode = 200;
-  String respMessage;
-
-  if(ssid.length() > 31 || ssid.length() == 0) {
-    respCode = 400;
-    respMessage = "Invalid SSID provided.";
-  } else if((pass.length() > 0 && pass.length() < 8) || pass.length() > 63) {
-    respCode = 400;
-    respMessage = "Invalid password provided.";
-  }
-
-  if(respCode == 200) {
-    WiFi.begin(ssid.c_str(), pass.c_str());
-
-    for(int t=0; t<MAX_TRIES && (status = WiFi.status()) != WL_CONNECTED; ++t) {
-      delay(200);
-    }
-
-    if(status != WL_CONNECTED) {
-      respCode = 403;
-      respMessage = "Invalid credentials provided.";
-    } else {
-      respCode = 200;
-      respMessage = "Connected succesfully to provided WiFi.";
-    }
-  }
-
-  PREi::sendJSON(respCode, respMessage);
-
-  if(respCode/100 != 2) {
-    WiFi.disconnect();
-  }
-}
-
-void PREi::handleDisconnect() {
-  PREi::sendJSON(200, "Disconnected succesfully!");
-  WiFi.disconnect();
 }
